be37ff060bca0c1948914a12da49b3e9e6aec8f7
unknown <viocan2005@gmail.com>
2021-11-27 18:07:57 +0300

    'add_testimonials-section'

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,5 @@
+window.swiper = Swiper;
+
 const headerMenu = () => {
 	const body = document.querySelector("body");
 	const headerBurgerElement = document.querySelector(".header__burger");
@@ -8,6 +10,39 @@ const headerMenu = () => {
 	});
 };
 
+const initSliders = () => {
+	new Swiper(".tst-slider-1", {
+		navigation:{
+			nextEl:".tst-slider__button-next",
+			prevEl:".tst-slider__button-prev"
+		},
+		slidesPerView:"auto",
+		// loop:true,
+		centeredSlides:true,
+		centeredSlidesBounds:true,
+		initialSlide:2,
+		spaceBetween:15,
+		breakpoints: {
+			767: {
+				slidesPerView: 2,
+				centeredSlides:false,
+				centeredSlidesBounds:false,
+			},
+			920:{
+				centeredSlides:true,
+				centeredSlidesBounds:true,
+			},
+			1200:{
+			  slidesPerView:3,
+			  centeredSlidesBounds:true,
+		    }
+		   },
+		
+	  });
+};
+
+
 document.addEventListener("DOMContentLoaded", function () {
 	headerMenu();
+	initSliders();
 });
